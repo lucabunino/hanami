29df2d6fdd6b0800f5c64555682a5bfa6b3b050d
lucabunino <66553738+lucabunino@users.noreply.github.com>
2024-12-05 18:44:01 +0100

    Favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,193 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section id="hero" class="bg-forest">
+  <h1 class="text-l">HanaMi: una scuola di agroecologia a cielo aperto</h1>
+</section>
+<a href="/#about" class="section-title bg-green" style="top: calc(var(--headerHeight) + var(--barHeight) * 0)">About</a>
+<section id="about">
+  <div class="bg-green">
+    <h3 class="text-m">HanaMi è cambiamento, trasformazione situata e coevolutiva. Si radica al margine periurbano, nel primo sistema agroforestale rigenerativo di Milano e qui vuole crescere, assieme ai giovani della città. Campo di apprendimento e sperimentazione, HanaMi è una non-scuola a cielo aperto dove fare, conoscere e abitare la transizione ecologica. Attraverso dispositivi teorici e strumenti realizzativi che permetteranno di conoscere il territorio e di custodirlo, HanaMi si fa rete antifragile di relazioni vicine e lontane.</h3>
+    <div class="grid">
+      <div class="grid-item">
+        <h4>Quando?</h4>
+        <p>da marzo a giugno</p>
+        <p>da settembre a novembre</p>
+      </div>
+      <div class="grid-item">
+        <h4>Dove?</h4>
+        <p><a href="https://maps.app.goo.gl/hZYy92LfvDyxRAbL8" target="_blank" rel="noopener noreferrer">via Roma 1, Milano</a></p>
+        <p>zona Corvetto</p>
+      </div>
+      <div class="grid-item">
+        <h4>Per chi?</h4>
+        <p>HanaMi è dedicato ai giovani dai 18 ai 35 anni, che siano o giovani laureandi o giovani laureati, o aspiranti imprenditori agroecologici o giovani interessati ai temi della transizione ecologica.</p>
+      </div>
+    </div>
+  </div>
+</section>
+<a href="/#corsi" class="section-title bg-forest" style="top: calc(var(--headerHeight) + var(--barHeight) * 1)">Corsi</a>
+<section id="corsi">
+  <div class="bg-forest">
+    <h3 class="text-m">Il programma didattico di HanaMi è suddiviso in due trimestri, da marzo a giugno e da settembre a novembre, con oltre 400 ore di lezioni, laboratori ed esercitazioni sul campo. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</h3>
+    <div class="grid">
+      <div class="grid-item">
+        <ul>
+          <li>96 ore di lezioni con docenti di Unimi e Polimi</li>
+          <li>96 ore di lezioni con professionisti ed esperti di agroecologia e architettura rigenerativa</li>
+          <li>96 ore di ﬂipped classroom, documentari e autoformazione</li>
+          <li>2 ettari di terreno da rigenerare con oltre 140 ore di pratica e workshop</li>
+        </ul>
+      </div>
+      <div class="grid-item">
+        <p>L’obiettivo è quello di approfondire temi cruciali della transizione ecologica, attraverso lezioni teoriche, workshop pratici e visite sul campo, suddivisi in 8 moduli.</p>
+      </div>
+    </div>
+    <div class="text-l">CORSI</div>
+  </div>
+</section>
+<a href="/#iscrizioni" class="section-title bg-green" style="top: calc(var(--headerHeight) + var(--barHeight) * 2)">Iscrizioni</a>
+<section id="iscrizioni">
+  <div class="bg-green">
+    <h3 class="text-m">HanaMi pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.</h3>
+    <div class="grid">
+      <div class="grid-item">
+        <h3 class="text-m">Procedura di iscrizione</h3>
+        <ol>
+          <li>Avvia la procedura accedendo al form di iscrizione che trovi al link sottostante.</li>
+          <li>HanaMi offre diverse modalità di iscrizione. Potrai scegliere se iscriverti al corso completo, ad un singolo modulo o ad una singola giornata.</li>
+          <li>Carica la documentazione richiesta, che trovi nella sezione “Documenti per l’Iscrizione” nell’apposita sezione.</li>
+          <li>Invia la tua application! Torneremo da te il prima possibile.<br>Se hai dubbi o domande, non esitare a contattarci all’indirizzo <a href="mailto:hanami@soulfoodforestfarms.it">hanami@soulfoodforestfarms.it</a></li>
+        </ol>
+      </div>
+      <div class="grid-item">
+        <h3 class="text-m">Documenti per l’iscrizione</h3>
+        <ul>
+          <li>Curriculum vitae in table form</li>
+          <li>Transcripts: Official transcripts from previously attended colleges, universities and any other post-secondary institutions</li>
+          <li>(Digital) portfolio with artistic and creative works</li>
+          <li>A short, written statement that the work you have submitted has been produced by your own hand</li>
+          <li> A passport photo in digital form</li>
+        </ul>
+        <p>Compila il form sottostante per inviare la tua candidatura e avviare la procedura di iscrizione al corso.</p>
+      </div>
+      <div class="grid-item">
+        <h3 class="text-m">Scadenze</h3>
+        <p class="deadline">31 Gennaio 2025</p>
+        <p>Deadline per invio della candidatura</p>
+      </div>
+      <a href="#" target="_blank" rel="noopener noreferrer" class="btn">Vai al form</a>
+    </div>
+  </div>
+</section>
+<a href="/#info" class="section-title bg-forest" style="top: calc(var(--headerHeight) + var(--barHeight) * 3)">Info & contatti</a>
+<section id="info">
+  <div class="bg-forest">
+    <p>HanaMi è un progetto che nasce all’interno di Soulfood Forestfarms.<br>Lorem ipsum dolor sit amet, hic hierant quisque sunt.</p>
+    <div class="grid">
+      <div class="grid-item">
+        <p><a href="mailto:hanami@soulfoodforestfarms.it">hanami@soulfoodforestfarms.it</a></p>
+        <p><a href="https://instagram.com/hanami_soulfood" target="_blank" rel="noopener noreferrer">@hanami_soulfood</a></p>
+        <p>T <a href="tel:+393478898281">+39 347 8898281</a></p>
+      </div>
+      <div class="grid-item">
+        <p><a href="mailto:hanami@soulfoodforestfarms.it">hanami@soulfoodforestfarms.it</a></p>
+        <p>Impresa Sociale S.r.l.</p>
+        <p><a href="https://maps.app.goo.gl/z5MQjd13DbRYh9zw8" target="_blank" rel="noopener noreferrer">via Druso 9, 20133 Milano</a></p>
+      </div>
+    </div>
+    <div class="grid">
+      <div class="grid-item">
+        <h4>Istituzioni</h4>
+        <ul>
+          <li><a href="https://www.polimi.it/" target="_blank" rel="noopener noreferrer">Politecnico di Milano</a></li>
+          <li><a href="https://www.unimi.it/" target="_blank" rel="noopener noreferrer">Università degli Studi di Milano</a></li>
+        </ul>
+      </div>
+      <div class="grid-item">
+        <h4>Professionisti</h4>
+        <ul>
+          <li><a href="#" target="_blank" rel="noopener noreferrer">Riccardo Gini</a></li>
+          <li><a href="https://terzopaesaggio.org/" target="_blank" rel="noopener noreferrer">Terzo Paesaggio</a></li>
+          <li><a href="https://www.biologicorigenerativo.it/" target="_blank" rel="noopener noreferrer">Iside Farm</a></li>
+          <li><a href="https://faunaviva.wordpress.com/" target="_blank" rel="noopener noreferrer">Fauna viva</a></li>
+          <li><a href="#" target="_blank" rel="noopener noreferrer">Del Toro</a></li>
+          <li><a href="#" target="_blank" rel="noopener noreferrer">Vairo</a></li>
+        </ul>
+      </div>
+      <div class="grid-item">
+        <h4>Organizzazioni</h4>
+        <ul>
+          <li><a href="https://soulfoodforestfarms.it/" target="_blank" rel="noopener noreferrer">Soulfood Forstfarms</a></li>
+          <li><a href="#" target="_blank" rel="noopener noreferrer">Altro</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="logos">
+      <h4>HanaMi é un progetto di</h4>
+      <div class="logos-row">
+        <img src="" alt="">
+      </div>
+      <h4>Con il contributo di</h4>
+      <div class="logos-row">
+        <img src="" alt="">
+        <img src="" alt="">
+        <img src="" alt="">
+        <img src="" alt="">
+      </div>
+    </div>
+  </div>
+</section>
+
+<style>
+section>div {
+  padding: 0 var(--margin);
+}
+.bg-forest {
+  background-color: var(--forest);
+}
+.bg-green {
+  background-color: var(--green);
+}
+#hero {
+  width: 100%;
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.section-title {
+  width: 100%;
+  padding: var(--gutter) var(--margin);
+  position: sticky;
+  z-index: 1;
+  text-transform: uppercase;
+}
+
+#about::before {
+  content: '';
+  display: block;
+  height: calc(var(--headerHeight) + var(--barHeight) * 1);
+  margin-top: calc((var(--headerHeight) + var(--barHeight) * 1)*-1);
+  visibility: hidden;
+}
+#corsi::before {
+  content: '';
+  display: block;
+  height: calc(var(--headerHeight) + var(--barHeight) * 2);
+  margin-top: calc((var(--headerHeight) + var(--barHeight) * 2)*-1);
+  visibility: hidden;
+}
+#iscrizioni::before {
+  content: '';
+  display: block;
+  height: calc(var(--headerHeight) + var(--barHeight) * 3);
+  margin-top: calc((var(--headerHeight) + var(--barHeight) * 3)*-1);
+  visibility: hidden;
+}
+#info::before {
+  content: '';
+  display: block;
+  height: calc(var(--headerHeight) + var(--barHeight) * 4);
+  margin-top: calc((var(--headerHeight) + var(--barHeight) * 4)*-1);
+  visibility: hidden;
+}
+</style>
